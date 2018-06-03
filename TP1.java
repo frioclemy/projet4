0054a0251fb8ff99c1763564b9bc68fd96a03156
frioclemy <fred@frioclemy.net>
2018-06-03 15:19:36 +0200

    first commit

---
+++ TP1.java
@@ -0,0 +1,21 @@
+package projet4;
+
+import java.util.*
+
+public class TP1 {
+
+    /**
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Methode main
+		 */
+		public static double arrondi(double A, int B) {
+			return (double) ( (int) (A * Math.pow(10, B) * .5)) / Math.pow(10, B);
+		}
+		
+		
+	}
+
+}
